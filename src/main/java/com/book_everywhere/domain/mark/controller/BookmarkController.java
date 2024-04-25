f4c5fd45eda726efc65781bb404e087277c2e4f2
kf3907 <rnjsrleo1@gmail.com>
2024-04-25 14:19:12 +0900

    fix : rest API 사용 안되는 문제 해결

--- src/main/java/com/book_everywhere/domain/mark/controller/BookmarkController.java
+++ src/main/java/com/book_everywhere/domain/mark/controller/BookmarkController.java
@@ -16,19 +16,19 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping("/api/bookmark/{socialId}")
-    public CMRespDto<?> findBookmarkWithUser(@RequestParam Long socialId) {
+    public CMRespDto<?> findBookmarkWithUser(@PathVariable Long socialId) {
         List<BookmarkDto> result = bookmarkService.유저_북마크_조회(socialId);
         return new CMRespDto<>(HttpStatus.OK, result, "유저 북마크 조회 성공");
     }
 
     @PostMapping("/api/bookmark")
-    public CMRespDto<?> saveBookmark(@PathVariable Long socialId, @PathVariable String address) {
+    public CMRespDto<?> saveBookmark(@RequestParam Long socialId, @RequestParam String address) {
         bookmarkService.북마크_등록(socialId, address);
         return new CMRespDto<>(HttpStatus.OK, null, "유저 북마크 등록 성공");
     }
 
     @DeleteMapping("/api/bookmark/{bookmarkId}")
-    public CMRespDto<?> deleteBookmark(@RequestParam Long bookmarkId) {
+    public CMRespDto<?> deleteBookmark(@PathVariable Long bookmarkId) {
         bookmarkService.북마크_삭제(bookmarkId);
         return new CMRespDto<>(HttpStatus.OK, null, "유저 북마크 삭제 성공");
     }
