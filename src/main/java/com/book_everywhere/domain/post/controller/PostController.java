07030bed8a6951cf6585b661bcfa9d21ac021b35
kf3907 <rnjsrleo1@gmail.com>
2024-05-03 19:53:11 +0900

    refactor: @Operation 설명 추가

--- src/main/java/com/book_everywhere/domain/post/controller/PostController.java
+++ src/main/java/com/book_everywhere/domain/post/controller/PostController.java
@@ -1,9 +1,11 @@
 package com.book_everywhere.domain.post.controller;
 
 import com.book_everywhere.common.dto.CMRespDto;
+import com.book_everywhere.common.dto.CountDto;
 import com.book_everywhere.domain.post.dto.PostReqDto;
 import com.book_everywhere.domain.post.dto.PostRespDto;
 import com.book_everywhere.domain.post.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,6 +20,7 @@ public class PostController {
     private final PostService postService;
 
     //전체 장소 리뷰 조회
+    @Operation(summary = "모든 큐레이팅 조회", description = "모든 큐레이팅 조회 return = List<PostRespDto>")
     @GetMapping("/api/posts")
     public CMRespDto<?> findAllPosts() {
         List<PostRespDto> result = postService.모든_장소_리뷰_조회();
@@ -25,6 +28,7 @@ public class PostController {
     }
 
     //단일 장소 리뷰 조회
+    @Operation(summary = "단일 큐레이팅 조회", description = "단일 큐레이팅 조회 return = PostRespDto")
     @GetMapping("/api/post/{postId}")
     public CMRespDto<?> findPost(@PathVariable Long postId) {
         PostRespDto result = postService.장소_리뷰_조회(postId);
@@ -32,16 +36,19 @@ public class PostController {
     }
 
     //유저 장소 리뷰 조회
+    @Operation(summary = "유저의 모든 큐레이팅 조회", description = "유저의 모든 큐레이팅을 개수와 함께 조회합니다. return = CountDto<List<PostRespDto>>")
     @GetMapping("/api/post/{socialId}")
     public CMRespDto<?> findUserPosts(@PathVariable Long socialId) {
-        List<PostRespDto> result = postService.유저의_모든_장소_리뷰_조회(socialId);
+        List<PostRespDto> postRespDtos = postService.유저의_모든_장소_리뷰_조회(socialId);
+        CountDto<?> result = new CountDto<>(postRespDtos);
         return new CMRespDto<>(HttpStatus.OK, result, "유저의 장소 리뷰 조회 성공!");
     }
 
-    //유저 큐레이팅 생성
+    //유저 큐레이팅 생성 (임시)
+    @Operation(summary = "큐레이팅 생성", description = "새 큐레이팅을 저장합니다.")
     @PostMapping("/api/post")
     public CMRespDto<?> savePost(@Valid @RequestBody PostReqDto postReqDto) {
-        postService.장소_리뷰_생성(postReqDto);
+        postService.장소_리뷰_생성(postReqDto); // 이미지 관련 부분 구현이 필요합니다!
         return new CMRespDto<>(HttpStatus.OK, null, "큐레이팅 저장 성공!");
     }
 
